05ec29f69f497079fbe2062f92659229953ec006
christopherdavi <67648870+christopherdavi@users.noreply.github.com>
2020-06-30 12:34:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo mivehiculo = new Vehiculo("kia", "kIC53", 2017,1054);
+            Console.WriteLine("La marca del vehiculo es: "+ mivehiculo.marca);
+            Console.WriteLine("La placa del vehiculo es: "+ mivehiculo.placa);
+            Console.WriteLine("El modelo del vehiculo es: "+ mivehiculo.modelo);
+            Console.WriteLine("El precio del vehiculo es:" + mivehiculo.precio);
+        }
+    }
+
+}
+ 
+    
---
+++ Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_Clase
+{
+    class Vehiculo
+    {
+        //propiedades
+        public string marca;
+        public string placa;
+        public int modelo;
+        public int precio;
+        //metodo
+        public Vehiculo(string marca, string placa, int modelo,int precio)
+        {
+            this.marca = marca;
+            this.placa = placa;
+            this.modelo = modelo;
+            this.precio = precio;
+        }
+    }
+}
